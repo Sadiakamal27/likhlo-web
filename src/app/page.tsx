7d53507bbadd5a45c265dc1558060bbbbe5cc126
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-07 01:58:05 +0500

    dark mode final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { FaRegMoon } from "react-icons/fa";
+import { useTheme } from "next-themes";
+import { FaRegMoon, FaRegSun } from "react-icons/fa";
 
 export default function Home() {
+  const { setTheme, theme } = useTheme();
   return (
     <main className="">
       <div>
         Likhlo
         <Button>Checkout</Button>
-        <Button size="icon" variant="outline">
-          <FaRegMoon className="h-4 w-4" />
+        <Button
+          onClick={() =>
+            theme == "dark" ? setTheme("light") : setTheme("dark")
+          }
+          variant="outline"
+          size="icon"
+        >
+          <FaRegSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <FaRegMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
       </div>
     </main>
