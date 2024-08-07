ed7ef81bcd8d51cfc943ac90250bd4daa3b49439
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-07 17:06:15 +0500

    icon swapped

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,6 +30,9 @@ export default function Navbar(): any {
           <Image src={logo} alt="logo" className="w-20 invert dark:invert-0" />
         </div>
         <div className="flex items-center gap-3  justify-self-end">
+          <Button size="icon" variant="default">
+            <FiEdit3 className="h-4 w-4" />
+          </Button>
           <Button
             onClick={() =>
               theme == "dark" ? setTheme("light") : setTheme("dark")
@@ -39,9 +42,6 @@ export default function Navbar(): any {
           >
             <FiSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <FiMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          </Button>
-          <Button size="icon" variant="default">
-            <FiEdit3 className="h-4 w-4" />
           </Button>
           <Button size="icon" variant="outline">
             <FiMoreVertical className="h-4 w-4" />
