b8dbb3e64adcfa2136778690312155e7b1ac76b5
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-07 14:33:55 +0500

    navbar grid

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,6 +9,7 @@ import {
   FiSun,
   FiMoon,
   FiMoreVertical,
+  FiShoppingBag,
 } from "react-icons/fi";
 import Image from "next/image";
 import { useTheme } from "next-themes";
@@ -16,12 +17,20 @@ import { useTheme } from "next-themes";
 export default function () {
   const { setTheme, theme } = useTheme();
   return (
-    <div className="border-b border-muted p-2">
-      <div className="flex w-full justify-between items-center">
+    <div className="border-b border-input p-2 ">
+      <div className="grid grid-cols-3">
         <div className="flex items-center gap-3">
+          <Button size="icon" variant="outline">
+            <FiShoppingBag className="h-4 w-4" />
+          </Button>
           <Button size="icon" variant="outline">
             <FiShoppingCart className="h-4 w-4" />
           </Button>
+        </div>
+        <div className="justify-self-center ">
+          <Image src={logo} alt="logo" className="w-16 invert dark:invert-0" />
+        </div>
+        <div className="flex items-center gap-3  justify-self-end">
           <Button
             onClick={() =>
               theme == "dark" ? setTheme("light") : setTheme("dark")
@@ -32,11 +41,7 @@ export default function () {
             <FiSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <FiMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           </Button>
-        </div>
-        <Image src={logo} alt="logo" className="w-16" />
-
-        <div className="flex items-center gap-3">
-          <Button size="icon" variant="outline">
+          <Button size="icon" variant="default">
             <FiEdit3 className="h-4 w-4" />
           </Button>
           <Button size="icon" variant="outline">
