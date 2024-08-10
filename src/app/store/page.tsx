5eb2aebabc80f77c9f9bc52d1e60e21012ca5371
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-10 19:32:37 +0500

    book dummy added

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Store() {
-  return <div>Store</div>;
+  return (
+    <div>
+      <div className="flex w-full p-3">
+        <div className="h-16 w-8 bg-muted/40"></div>
+      </div>
+    </div>
+  );
 }
