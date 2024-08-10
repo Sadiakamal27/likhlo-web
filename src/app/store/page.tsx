b0db56f222630ede985421884f4e8a76f1dced34
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-10 19:50:53 +0500

    book card compoonent

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,11 +1,10 @@
+import BookCard from "@/components/book-card";
 import React from "react";
 
 export default function Store() {
   return (
     <div>
-      <div className="flex w-full p-3">
-        <div className="h-64 w-48 bg-muted/40 rounded-lg"></div>
-      </div>
+      <BookCard />
     </div>
   );
 }
---
+++ src/components/book-card.tsx
@@ -0,0 +1,7 @@
+export default function BookCard() {
+  return (
+    <div className="flex w-full p-3">
+      <div className="h-64 w-48 bg-muted/40 rounded-lg"></div>
+    </div>
+  );
+}
