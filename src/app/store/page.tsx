a698f9518b019e8842cf70fd99a85149f1df8d0e
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-10 19:38:05 +0500

    book dummy final

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,7 +4,7 @@ export default function Store() {
   return (
     <div>
       <div className="flex w-full p-3">
-        <div className="h-16 w-8 bg-muted/40"></div>
+        <div className="h-64 w-48 bg-muted/40 rounded-lg"></div>
       </div>
     </div>
   );
