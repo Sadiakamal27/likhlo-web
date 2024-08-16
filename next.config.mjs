a9011bee61cc40f3c2131c801be990691f33c7c5
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-16 23:13:33 +0500

    route redirect in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirect() {
+    return [
+      {
+        source: "/",
+        destination: "/store",
+        permanent: "true",
+      },
+    ];
+  },
+};
 export default nextConfig;
