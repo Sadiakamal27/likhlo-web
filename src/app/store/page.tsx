248dcae53231749fba1237b7f1bb9cbf45f018fd
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-17 00:02:26 +0500

    side actions added on store

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,18 +1,54 @@
 import BookCard from "@/components/book-card";
-import Navbar from "@/components/navbar";
+import { FiFilter, FiGrid, FiSearch, FiShoppingBag } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
 
 export default function Store() {
   return (
     <main>
-      <div className="flex ">
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
-        <BookCard />
+      <div className="px-4 flex flex-col  gap-4 fixed">
+        <Button size="icon" variant="secondary">
+          <FiSearch className="h-4 w-4" />
+        </Button>
+        <Button size="icon" variant="secondary">
+          <FiFilter className="h-4 w-4" />
+        </Button>
+        <Button size="icon" variant="secondary">
+          <FiGrid className="h-4 w-4" />
+        </Button>
       </div>
+
+      <section className="pl-20">
+        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg">
+          Top Sellers
+          <div className="flex  gap-4 justify-between">
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+        </div>
+        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg">
+          Top Sellers
+          <div className="flex  gap-4 justify-between">
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+        </div>
+        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg">
+          Top Sellers
+          <div className="flex  gap-4 justify-between">
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
