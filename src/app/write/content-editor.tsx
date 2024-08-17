0406b14eae8f8719e7d8ca87daa987ecbccff16e
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-18 02:16:43 +0500

    content-editor init

---
+++ src/app/write/content-editor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ContentEditor() {
+  return (
+    <div>
+      <div>Description: hello</div>
+
+      <div>Contents</div>
+    </div>
+  );
+}
--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import BookMeta from "./book-meta";
+import ContentEditor from "./content-editor";
 
 export default function () {
   return (
     <>
       <div>
         <BookMeta />
+        <ContentEditor />
       </div>
     </>
   );
