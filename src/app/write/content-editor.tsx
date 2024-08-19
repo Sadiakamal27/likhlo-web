8d1aae5cbac8c38df501a262659f44c17c098f16
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-19 21:21:50 +0500

    blocks for chapters

--- src/app/write/content-editor.tsx
+++ src/app/write/content-editor.tsx
@@ -7,7 +7,33 @@ export default function ContentEditor({ className }: { className?: string }) {
   return (
     <div className={className}>
       <div>Description: hello</div>
-      <Card className="flex flex-col gap-2 w-full p-2">
+      <Card className="flex mt-4 flex-col gap-2 w-full p-2">
+        <div className="flex gap-2 justify-end">
+          <ContentBlock
+            className="w-full flex justify-between"
+            variant="outline"
+          >
+            <p>Chapter 1</p>
+            <FiArrowUpRight className="w-4 h-4" />
+          </ContentBlock>
+          <Button variant="outline" size="icon">
+            <FiPlus className="h-4 w-4" />
+          </Button>
+        </div>
+        <div className="flex gap-2 justify-end">
+          <ContentBlock
+            className="w-11/12  flex  justify-between"
+            variant="ghost"
+          >
+            <p>Topic 1</p>
+            <FiArrowUpRight className="w-4 h-4" />
+          </ContentBlock>
+          <Button variant="ghost" size="icon">
+            <FiPlus className="h-4 w-4" />
+          </Button>
+        </div>{" "}
+      </Card>
+      <Card className="flex mt-4 flex-col gap-2 w-full p-2">
         <div className="flex gap-2 justify-end">
           <ContentBlock
             className="w-full flex justify-between"
