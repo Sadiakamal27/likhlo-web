6e14cd7a3a8314947e637477bba5e6272cce58d7
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-25 17:18:19 +0500

    separator added

--- src/app/write/content-editor.tsx
+++ src/app/write/content-editor.tsx
@@ -1,18 +1,20 @@
-import { FiEdit, FiEdit2, FiPlus, FiTrash } from "react-icons/fi";
+import { FiEdit2, FiPlus, FiTrash } from "react-icons/fi";
 import ContentCard from "./content-card";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function ContentEditor({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <div className="text-foreground">
+      <div className="text-foreground  flex flex-col gap-4 ">
         <h2 className="p-0 text-xl font-bold ">Description</h2>
 
         <textarea
-          className="bg-background w-full h-auto outline-none "
+          className="bg-background max-h-32 placeholder:text-foreground/30 w-full h-auto outline-none "
           placeholder="Enter book description here..."
         />
       </div>
+      <Separator className="my-6" />
       <div className="text-foreground">
         <div className="flex justify-between items-center ">
           <h2 className="p-0 m-0 h-full text-3xl font-bold ">Contents</h2>
