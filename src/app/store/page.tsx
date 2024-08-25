94ccdc9e018b7c38201017dacc435cc0b6157fce
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-25 14:00:40 +0500

    layout edits in store page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -5,8 +5,8 @@ import Dock from "../write/dock";
 
 export default function Store() {
   return (
-    <main>
-      <div className="px-4 flex flex-col  gap-4 fixed">
+    <main className=" flex  flex-row max-sm:flex-col max-sm:items-center items-start  relative ">
+      <div className="px-4  basis-1/5 flex flex-col max-sm:flex-row    gap-4 sticky top-20">
         <Button size="icon" variant="secondary">
           <FiSearch className="h-4 w-4" />
         </Button>
@@ -17,16 +17,27 @@ export default function Store() {
           <FiGrid className="h-4 w-4" />
         </Button>
       </div>
-      <section className="pl-20">
+      <section className="basis-5 w-auto">
         {" "}
-        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
+        <div className="flex flex-col max-w-min bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
           Top Sellers
           <div className="flex  gap-4 justify-between">
             <BookCard />
             <BookCard />
             <BookCard />
             <BookCard />
+          </div>
+        </div>
+        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
+          Top Sellers
+          <div className="flex  gap-4 justify-between">
             <BookCard />
+            <BookCard />
+          </div>
+        </div>
+        <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
+          Top Sellers
+          <div className="flex  gap-4 justify-between">
             <BookCard />
             <BookCard />
             <BookCard />
