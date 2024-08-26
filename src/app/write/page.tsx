0704bec4123ddfd408b602dbb197dbac943d58cb
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-26 16:51:51 +0500

    gradients added in both themes

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/ui/card";
 export default function Write() {
   return (
     <>
-      <Card className="container bg-muted/30 my-8  h-auto grid grid-cols-3 gap-8  max-sm:grid-cols-1 py-6 ">
+      <Card className="container bg-muted/30 backdrop-blur-2xl my-8  h-auto grid grid-cols-3 gap-8  max-sm:grid-cols-1 py-6 ">
         <BookMeta />
         <BookEditor className="col-span-2 max-sm:col-span-1" />
       </Card>
