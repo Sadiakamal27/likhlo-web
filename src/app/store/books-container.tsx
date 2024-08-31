04d02699bcc577b3525896e902de935319a5118d
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-31 23:54:19 +0500

    map func for book card in store page

--- src/app/store/books-container.tsx
+++ src/app/store/books-container.tsx
@@ -5,6 +5,10 @@ export default function BookContainer() {
     <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
       Top Sellers
       <div className="flex  gap-4 justify-between">
+        {books.map((book) => {
+          <BookCard data={book} />;
+        })}
+
         <BookCard />
         <BookCard />
         <BookCard />
