1b0bd8e0fd44cbca67953bb1e40b3a3ada69c121
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-06 01:24:18 +0500

    dynamic book content

--- src/app/write/content-card.tsx
+++ src/app/write/content-card.tsx
@@ -1,37 +1,19 @@
 import { Card } from "@/components/ui/card";
 import { ContentItem } from "@/components/content-item";
 import { FiArrowUpRight } from "react-icons/fi";
+import { books } from "../data";
 
 export default function ContentCard() {
   return (
-    <Card className="flex bg-transparent mt-4 flex-col gap-2 w-full rounded-2xl p-2">
-      {/* Chapter block*/}
-      <ContentItem size="chapter">
-        <p>Chapter 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-      {/* topic block*/}
-      <ContentItem variant="outline" size="topic">
-        <p>Topic 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-      <ContentItem variant="outline" size="topic">
-        <p>Topic 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-      {/*subtopic block*/}
-      <ContentItem variant="ghost" size="subtopic">
-        <p>Topic 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-      <ContentItem variant="ghost" size="subtopic">
-        <p>Topic 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-      <ContentItem variant="ghost" size="subtopic">
-        <p>Topic 1</p>
-        <FiArrowUpRight className="w-4 h-4" />
-      </ContentItem>
-    </Card>
+    {
+      books[0].content.map(
+        book:any => (
+      <div>
+chapter 1
+            </div>
+        )
+      )
+    }
+
   );
 }
