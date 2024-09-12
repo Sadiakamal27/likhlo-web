4f3f6bfc347a67b0957118b2ec25f9c81dde796c
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-12 23:57:43 +0500

    dynamic chapters and topics in content

--- src/components/content-card.tsx
+++ src/components/content-card.tsx
@@ -1,16 +1,20 @@
 import { Card } from "@/components/ui/card";
 import { ContentItem } from "@/components/content-item";
 import { FiArrowUpRight } from "react-icons/fi";
+import { books } from "@/app/data";
 
 export default function ContentCard() {
   return (
     <Card className="flex bg-transparent mt-4 flex-col gap-2 w-full rounded-2xl p-2">
       {/* Chapter block*/}
-      <ContentItem size="chapter">
+        {
+        books[0].content.map(chapter=>{
+        <ContentItem size="chapter">
         <p>Chapter 1</p>
         <FiArrowUpRight className="w-4 h-4" />
       </ContentItem>
-      {/* topic block*/}
+      {})
+      }/* topic block*/}
       <ContentItem variant="outline" size="topic">
         <p>Topic 1</p>
         <FiArrowUpRight className="w-4 h-4" />
