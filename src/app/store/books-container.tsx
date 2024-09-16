cf6e04b79e20c95dbbcb0ba5d8257a9ad19f1121
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-16 23:56:20 +0500

    max width to home book container

--- src/app/store/books-container.tsx
+++ src/app/store/books-container.tsx
@@ -2,7 +2,7 @@ import BookCard from "@/components/book-card";
 import { books } from "@/app/data";
 export default function BookContainer() {
   return (
-    <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden">
+    <div className="flex flex-col bg-muted/30 p-4 m-6 border border-muted/70 rounded-lg overflow-hidden max-w-16">
       Top Sellers
       <div className="flex  gap-4 justify-between">
         {books.map((book, index: number): any => (
