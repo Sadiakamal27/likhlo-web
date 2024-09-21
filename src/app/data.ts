068da762cf493b3981f5bcd32fadc85d57ca1072
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-21 23:29:16 +0500

    book data updated

--- src/app/data.ts
+++ src/app/data.ts
@@ -49,6 +49,40 @@ export const books: Book[] = [
     ],
   },
   {
+    title: "Outlier",
+    author: ["Ammar Qurshi"],
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1653065920i/59892260.jpg",
+    content: [
+      {
+        chapterTitle: "Cricket players",
+        topics: [
+          {
+            topicTitle: "Do what comes to your mind",
+            subtopic: [
+              {
+                subtopicTitle: "Why do this?",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        chapterTitle: "Hockey players",
+        topics: [
+          {
+            topicTitle: "Do what comes to your mind",
+            subtopic: [
+              {
+                subtopicTitle: "Why do this?",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
     title: "Blink",
     author: ["malcolm gladwell"],
     cover:
