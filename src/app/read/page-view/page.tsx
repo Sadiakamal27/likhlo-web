cef06709bf1fbe64608d634e30e9cfef4f50187d
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-25 23:13:06 +0500

    reading nav comp created

--- src/app/read/page-view/page.tsx
+++ src/app/read/page-view/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReadingNav from "./reading-nav";
 
 export default function PageView() {
   return (
---
+++ src/app/read/page-view/reading-nav.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReadingNav() {
+  return <div>ReadingNav</div>;
+}
