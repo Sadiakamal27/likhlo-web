5172de2165c43f73c06d7db9d8915162a9af24fd
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-06 17:21:36 +0500

    book prop type error fixed

--- src/components/content-card-list.tsx
+++ src/components/content-card-list.tsx
@@ -3,10 +3,10 @@ import { ContentItem } from "@/components/content-item";
 import { FiArrowUpRight } from "react-icons/fi";
 import { Book } from "@/app/data";
 
-export default function ContentCardList(book: { book: Book }) {
+export default function ContentCardList({ book }: { book: Book }) {
   return (
     <>
-      {book.chapters?.map((chapter) => {
+      {book.chapters?.map((chapter) => (
         <Card className="flex bg-transparent mt-4 flex-col gap-2 w-full rounded-2xl p-2">
           <ContentItem size="chapter">
             <p>{chapter.chapterTitle}</p>
@@ -26,8 +26,8 @@ export default function ContentCardList(book: { book: Book }) {
               ))}
             </>
           ))}
-        </Card>;
-      })}
+        </Card>
+      ))}
     </>
   );
 }
