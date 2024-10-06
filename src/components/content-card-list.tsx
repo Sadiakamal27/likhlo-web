31d96caf52501d7f8a4f194ce8ec077fab7bb534
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-06 18:04:32 +0500

    key props in content card list

--- src/components/content-card-list.tsx
+++ src/components/content-card-list.tsx
@@ -6,20 +6,23 @@ import { Book } from "@/app/data";
 export default function ContentCardList({ book }: { book: Book }) {
   return (
     <>
-      {book.chapters?.map((chapter) => (
-        <Card className="flex bg-transparent mt-4 flex-col gap-2 w-full rounded-2xl p-2">
+      {book.chapters?.map((chapter, index) => (
+        <Card
+          key={index}
+          className="flex bg-transparent mt-4 flex-col gap-2 w-full rounded-2xl p-2"
+        >
           <ContentItem size="chapter">
             <p>{chapter.chapterTitle}</p>
             <FiArrowUpRight className="w-4 h-4" />
           </ContentItem>
-          {chapter.topics?.map((topic) => (
+          {chapter.topics?.map((topic, index) => (
             <>
-              <ContentItem variant="secondary" size="topic">
+              <ContentItem key={index} variant="secondary" size="topic">
                 <p>{topic.topicTitle}</p>
                 <FiArrowUpRight className="w-4 h-4" />
               </ContentItem>
-              {topic.subtopics?.map((subtopic) => (
-                <ContentItem variant="ghost" size="subtopic">
+              {topic.subtopics?.map((subtopic, index) => (
+                <ContentItem key={index} variant="ghost" size="subtopic">
                   <p>{subtopic.subtopicTitle}</p>
                   <FiArrowUpRight className="w-4 h-4" />
                 </ContentItem>
