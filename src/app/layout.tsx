b5c3d35a84f4bcb18fd1588cc35f99cd88962869
ammarrqureshi <mamqureshi786@gmail.com>
2024-10-12 21:55:04 +0500

    chores in style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import bgImageDark from "@/assets/mesh-gradient-1.png";
-import bgImageLight from "@/assets/mesh-gradient-light.png";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider, useTheme } from "next-themes";
+import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/navbar";
 import Image from "next/image";
 const quicksand = Quicksand({ subsets: ["latin"] });
